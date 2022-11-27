ff18fb44e76eb88921519e20ed2657bac939a380
Ashish Bhintade <68814285+ashishbhintade@users.noreply.github.com>
2022-11-27 17:19:33 +0530

    Local Storage

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,10 +1,11 @@
 import React, { createContext, useReducer } from "react";
+import { useEffect } from "react";
 import AppReducer from "./AppReducer";
 
 //Initial State
-const initialState = {
-  transactions: [],
-};
+const initialState = JSON.parse(localStorage.getItem("transactions"))
+  ? JSON.parse(localStorage.getItem("transactions"))
+  : { transactions: [] };
 
 //Create context
 export const GlobalContext = createContext(initialState);
@@ -12,7 +13,9 @@ export const GlobalContext = createContext(initialState);
 // Provider component
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
-
+  useEffect(() => {
+    localStorage.setItem("transactions", JSON.stringify(state));
+  }, [state]);
   function deleteTransaction(id) {
     dispatch({
       type: "DELETE_TRANSACTION",
